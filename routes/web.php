ae0c28932ab239e5f11b06fb5b28165a71ea3da0
ardie069 <ardi.pulaem069@gmail.com>
2025-01-18 16:57:51 +0700

    perubahan rute untuk menggunakan template pada halaman buttons, cards, colors, borders, animations, other, register, forgot-password, 404, charts, dan tables

--- routes/web.php
+++ routes/web.php
@@ -6,41 +6,41 @@ Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 Route::get('/buttons', function () {
-    return view('buttons');
+    return view('template.buttons');
 })->name('buttons');
 Route::get('/cards', function () {
-    return view('cards');
+    return view('template.cards');
 })->name('cards');
 Route::get('/colors', function () {
-    return view('utilities_color');
+    return view('template.utilities_color');
 })->name('colors');
 Route::get('/borders', function () {
-    return view('utilities_border');
+    return view('template.utilities_border');
 })->name('borders');
 Route::get('/animations', function () {
-    return view('utilities_animation');
+    return view('template.utilities_animation');
 })->name('animations');
 Route::get('/other', function () {
-    return view('utilities_other');
+    return view('template.utilities_other');
 })->name('other');
 Route::get('/login', function () {
     return view('login');
 })->name('login');
 Route::get('/register', function () {
-    return view('register');
+    return view('template.register');
 })->name('register');
 Route::get('/forgot-password', function () {
-    return view('forgot_password');
+    return view('template.forgot_password');
 })->name('forgot_password');
 Route::get('/404', function () {
-    return view('404');
+    return view('template.404');
 })->name('404');
 Route::get('/blank', function () {
     return view('blank');
 })->name('blank');
 Route::get('/charts', function () {
-    return view('charts');
+    return view('template.charts');
 })->name('charts');
 Route::get('/tables', function () {
-    return view('tables');
+    return view('template.tables');
 })->name('tables');
