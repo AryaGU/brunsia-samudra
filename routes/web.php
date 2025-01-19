46e2b281434bdf6fdde0bb2628a05983c37e2b6b
ardie069 <ardi.pulaem069@gmail.com>
2025-01-19 20:32:52 +0700

    refactor routes to use controller methods for improved organization and maintainability

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('admin.dashboard');
-})->name('dashboard');
-Route::get('/data-pengiriman', function () {
-    return view('admin.data_pengiriman');
-})->name('data_pengiriman');
-Route::get('/data-pengiriman/tambah', function () {
-    return view('admin.tambah_data');
-})->name('data_pengiriman.tambah');
-Route::get('/buttons', function () {
-    return view('template.buttons');
-})->name('buttons');
-Route::get('/cards', function () {
-    return view('template.cards');
-})->name('cards');
-Route::get('/colors', function () {
-    return view('template.utilities_color');
-})->name('colors');
-Route::get('/borders', function () {
-    return view('template.utilities_border');
-})->name('borders');
-Route::get('/animations', function () {
-    return view('template.utilities_animation');
-})->name('animations');
-Route::get('/other', function () {
-    return view('template.utilities_other');
-})->name('other');
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-Route::get('/register', function () {
-    return view('template.register');
-})->name('register');
-Route::get('/forgot-password', function () {
-    return view('template.forgot_password');
-})->name('forgot_password');
-Route::get('/404', function () {
-    return view('template.404');
-})->name('404');
-Route::get('/blank', function () {
-    return view('blank');
-})->name('blank');
-Route::get('/charts', function () {
-    return view('template.charts');
-})->name('charts');
-Route::get('/tables', function () {
-    return view('template.tables');
-})->name('tables');
+Route::get('/admin', [AdminController::class, 'dashboard'])->name('dashboard');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/admin/data-pengiriman', [AdminController::class, 'data_pengiriman'])->name('data_pengiriman');
+Route::get('/admin/data-pengiriman/tambah', [AdminController::class, 'tambah_data_pengiriman'])->name('data_pengiriman.tambah');
