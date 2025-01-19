dbe1274910055cc39997a887acda14565dddf006
ardie069 <ardi.pulaem069@gmail.com>
2025-01-19 21:12:48 +0700

    refactor routes to group admin routes under auth

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', [AdminController::class, 'dashboard'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('/admin/data-pengiriman', [AdminController::class, 'data_pengiriman'])->name('data_pengiriman');
+    Route::get('/admin/data-pengiriman/tambah', [AdminController::class, 'tambah_data_pengiriman'])->name('data_pengiriman.tambah');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/admin/data-pengiriman', [AdminController::class, 'data_pengiriman'])->name('data_pengiriman');
-Route::get('/admin/data-pengiriman/tambah', [AdminController::class, 'tambah_data_pengiriman'])->name('data_pengiriman.tambah');
+Route::post('/login', [AuthController::class, 'storeLogin'])->name('login.store');
